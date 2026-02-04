7579a593153388f9ec3173dc7a74b7ab0cca5aec
ВьLachezar Uzunov <lachezar.z.uzunov@gmail.com>
2026-02-04 10:55:51 +0200

    add: small changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,19 @@
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 const App = () => {
     return (
-        <div>App</div>
+        <>
+            <Header />
+            <main className='py-3'>
+                <Container>
+                    <h1>Welcome to Rock Shop</h1>
+                    <h2>This is a nice shop</h2>
+                </Container>
+            </main>
+            <Footer />
+        </>
     )
 }
 
